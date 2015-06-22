bebe93e62b03c7406cdfd53cf6673ef30d70d3e4
Nirvana <kimqqyun@gmail.com>
2015-06-23 01:55:38 +0900

    jinja2, codecs

--- flask_skeleton.py
+++ flask_skeleton.py
@@ -5,10 +5,14 @@ import os
 import click
 import argparse
 import shutil
+import jinja2
+import codecs
 
 cwd = os.getcwd()
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
+template_loader = jinja2.FileSystemLoader(searchpath=os.path.join(script_dir, "templates"))
+template_env = jinja2.Environment(loader=template_loader)
 
 def main(argv):
     # Arguments #
@@ -27,6 +31,14 @@ def main(argv):
     # Copy files and folders
     shutil.copytree(os.path.join(script_dir, skeleton_dir), fullpath)
 
+    # Create config.py
+    secret_key = codecs.encode(os.urandom(32), 'hex').decode('utf-8')
+    template = template_env.get_template('config.jinja2')
+    template_var = {
+        'secret_key': secret_key,
+    }
+    with open(os.path.join(fullpath, 'project', 'config.py'), 'w') as fd:
+        fd.write(template.render(template_var))
 
 if __name__ == '__main__':
     main(sys.argv)
