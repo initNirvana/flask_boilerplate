8acbeea3ee312a3f08ee7b4de19bcb21778873ed
Nirvana <kimqqyun@gmail.com>
2015-06-23 02:04:10 +0900

    add bower env

--- flask_skeleton.py
+++ flask_skeleton.py
@@ -8,6 +8,21 @@ import shutil
 import jinja2
 import codecs
 
+if sys.version_info < (3, 0):
+    from shutilwhich import which
+else:
+    from shutil import which
+
+output, error = subprocess.Popen(
+    [bower_exe, 'install', dependency],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE
+).communicate()
+# print(output)
+if error:
+    print("An error occurred with Bower")
+    print(error)
+
 cwd = os.getcwd()
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -19,6 +34,8 @@ def main(argv):
     parser = argparse.ArgumentParser(description='initNirvana\'s flask skeleton project')
     parser.add_argument('appname', help='The application name')
     parser.add_argument('-s', '--skeleton', help='The skeleton folder to use.')
+    parser.add_argument('-b', '--bower', help='Install dependencies via bower')
+
     args = parser.parse_args()
 
     # Variables #
@@ -39,6 +56,25 @@ def main(argv):
     }
     with open(os.path.join(fullpath, 'project', 'config.py'), 'w') as fd:
         fd.write(template.render(template_var))
+    # Add bower dependencies
+    if args.bower:
+        bower = args.bower.split(',')
+        bower_exe = which('bower')
+        if bower_exe:
+            os.chdir(os.path.join(fullpath, 'project', 'static'))
+            for dependency in bower:
+                output, error = subprocess.Popen(
+                    [bower_exe, 'install', dependency],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE
+                ).communicate()
+                # print(output)
+                if error:
+                    print("An error occurred with Bower")
+                    print(error)
+        else:
+            print("Could not find bower. Ignoring.")
+
 
 if __name__ == '__main__':
     main(sys.argv)
